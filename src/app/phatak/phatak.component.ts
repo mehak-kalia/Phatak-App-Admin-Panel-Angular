4f57cba07d8de0a578bef6e40134eaa77c8d8cfd
mehak-kalia <mehakkalia77@gmail.com>
2022-08-24 21:57:10 +0530

    Form Firebase connection

--- src/app/phatak/phatak.component.ts
+++ src/app/phatak/phatak.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 import { FormControl, FormGroup } from '@angular/forms';
+import { collection, doc, getDocs, setDoc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-phatak',
@@ -21,14 +23,43 @@ export class PhatakComponent implements OnInit {
     }
   ); 
 
-  constructor() { }
+  phataksList 
+
+  constructor(
+    private firestore: Firestore
+  ) { 
+   
+  }
 
   ngOnInit(): void {
+    let collectionRef = collection(this.firestore, "crossings")
+    getDocs(collectionRef).then((value)=> {
+      console.log(value.docs.map(e => ({id: e.id, ...e.data()})))
+    })
   }
 
   addPhatakToFirebase(){
     console.log("Function Add Executed");
     console.log(this.phatakForm.value);
+    //let value = this.phatakForm.value;
+    let value = Object.assign({}, this.phatakForm.value)
+
+
+    value['phatakId'] = doc(collection(this.firestore, "crossings")).id;
+    let docRef = doc(this.firestore, "crossings/" + value['phatakId'])
+    setDoc(docRef, value)
+    .then(() => {
+      console.log("Saved");
+      this.phatakForm.reset()
+    }, (error) => {
+      console.error(error);
+      
+    })
+
+    console.log(value);
+    
+
+    
 
   }
 
