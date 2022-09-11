e8fad1fa2753db016cf7a6467ed36957b6f5de3b
mehak-kalia <mehakkalia77@gmail.com>
2022-09-11 21:40:38 +0530

    previous additions

--- src/app/phatak/phatak.component.ts
+++ src/app/phatak/phatak.component.ts
@@ -1,7 +1,8 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
-import { FormControl, FormGroup } from '@angular/forms';
-import { collection, doc, getDocs, setDoc } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, Firestore, getDocs, onSnapshot, setDoc, Timestamp } from '@angular/fire/firestore';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { values } from 'cypress/types/lodash';
 
 @Component({
   selector: 'app-phatak',
@@ -10,57 +11,139 @@ import { collection, doc, getDocs, setDoc } from 'firebase/firestore';
 })
 export class PhatakComponent implements OnInit {
 
-  showForm = true;
-
+  showForm: boolean = false;
   phatakForm = new FormGroup(
     {
       phatakname: new FormControl(''),
-      inchargename: new FormControl(''),
+      personInChargeName: new FormControl(''),
       inchargephone: new FormControl(''),
       latitude: new FormControl(''),
       longitude: new FormControl(''),
-      phatakimage: new FormControl('')
+      imageUrl: new FormControl(''),
+      phatakId: new FormControl(''),
+      status: new FormControl(1),
+      timings: new FormArray([])
     }
   ); 
 
-  phataksList 
+  phataksList: any[] = [];
 
   constructor(
     private firestore: Firestore
-  ) { 
-   
-  }
+  ) { }
 
   ngOnInit(): void {
-    let collectionRef = collection(this.firestore, "crossings")
-    getDocs(collectionRef).then((value)=> {
-      console.log(value.docs.map(e => ({id: e.id, ...e.data()})))
+    //Read Operation
+    let collectionRef = collection(this.firestore, "crossings");
+    onSnapshot(collectionRef, (value) => {
+      this.phataksList = value.docs.map(e => ({ id: e.id, ...e.data() }));
+    }, (error) => {
+      console.log(error);
+      
     })
+    // getDocs(collectionRef).then((value) => {
+    //   // console.log();
+    //   this.phataksList = value.docs.map(e => ({ id: e.id, ...e.data() }));
+    // })
   }
 
+  getTimingsArrayFromPhatakForm() {
+    return this.phatakForm.get('timings') as FormArray;
+  }
+
+  addTimingDetailsToArray() {
+    this.getTimingsArrayFromPhatakForm().push(new FormGroup({
+      time: new FormControl(null),
+      trafficStatus: new FormControl(''),
+      train: new FormControl('')
+    }))
+  }
+
+  removeTimingDetailsToArray(idx: number) {
+    this.getTimingsArrayFromPhatakForm().removeAt(idx);
+  }
+
+  // Create Operation
   addPhatakToFirebase(){
     console.log("Function Add Executed");
-    console.log(this.phatakForm.value);
-    //let value = this.phatakForm.value;
-    let value = Object.assign({}, this.phatakForm.value)
-
-
-    value['phatakId'] = doc(collection(this.firestore, "crossings")).id;
-    let docRef = doc(this.firestore, "crossings/" + value['phatakId'])
-    setDoc(docRef, value)
+    let value: any = {...this.phatakForm.value};
+    
+    // addDoc(collection(this.firestore, "phataks"), value)
+    let phatakInfo = {
+      phatakId: value?.phatakId.length === 0 ? doc(collection(this.firestore, "crossings")).id : value.phatakId,
+      location: [value.latitude, value.longitude],
+      phatakName: value.phatakname,
+      inchargeName: value.inchargename,
+      inchargePhone: value.inchargephone,
+      status: value.status,
+      timings: value.timings.map(e => ({
+        trafficStatus: e.trafficStatus,
+        train: e.train,
+        time: Timestamp.fromDate(new Date(e.time))
+      })),
+      phatakImage: value.phatakimage
+    }
+    let docRef = doc(this.firestore, "crossings/" + phatakInfo.phatakId)
+    setDoc(docRef, phatakInfo)
     .then(() => {
       console.log("Saved");
-      this.phatakForm.reset()
+      this.phatakForm.reset({});
+      this.showForm = !this.showForm;
     }, (error) => {
       console.error(error);
       
     })
+  }
 
-    console.log(value);
-    
+  updatePhatak(phatak: any) {
+    this.showForm = true;
+    let datepipe = new DatePipe('en-US');
+    this.phatakForm = new FormGroup({
+      phatakname: new FormControl(phatak.phatakName),
+      personInChargeName: new FormControl(phatak.inchargeName),
+      inchargephone: new FormControl(phatak.inchargePhone),
+      latitude: new FormControl(phatak.location[0]),
+      longitude: new FormControl(phatak.location[1]),
+      imageUrl: new FormControl(phatak.imageUrl),
+      phatakId: new FormControl(phatak.phatakId),
+      status: new FormControl(phatak.status),
+      timings: new FormArray(phatak.timings.length === 0 ? [] : phatak.timings.map(element => new FormGroup({
+        time: new FormControl(datepipe.transform(element.time.toDate(), 'yyyy-MM-dd HH:mm')),
+        trafficStatus: new FormControl(element.trafficStatus),
+        train: new FormControl(element.train)
+      })))
+    });
 
-    
 
+    // this.phatakForm.patchValue({
+    //   phatakname: phatak.phatakName,
+    //   inchargename: phatak.inchargeName,
+    //   inchargephone: phatak.inchargePhone,
+    //   latitude: phatak.location[0],
+    //   longitude: phatak.location[1],
+    //   phatakimage: phatak.phatakImage,
+    //   phatakId: phatak.phatakId,
+    //   status: phatak.status,
+    // });
+
+    // phatak.timings.forEach(element => {
+    //   // console.log(element);
+    //   this.getTimingsArrayFromPhatakForm().push(new FormGroup({
+    //     time: new FormControl(datepipe.transform(element.time.toDate(), 'yyyy-MM-dd HH:mm')),
+    //     trafficStatus: new FormControl(element.trafficStatus),
+    //     train: new FormControl(element.train)
+    //   }))
+    // });
+  }
+
+  deletePhatak(phatakId: string) {
+    let docRef = doc(this.firestore, "crossings/" + phatakId);
+    deleteDoc(docRef).then(() => {
+      console.log("Delete Successfully");
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 
 }
